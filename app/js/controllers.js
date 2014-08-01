97591e52465d4d89d3ed79c4fedda4a5fc4007a6
subhodeep-cuelogic <subhodeep.majumder@cuelogic.co.in>
2014-08-01 13:08:12 +0530

    created a hello world assignment

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,9 +4,12 @@
 
 angular.module('myApp.controllers', [])
   .controller('MyCtrl1', ['$scope', function($scope) {
-  	$scope.hello ="Test";
+  	   $scope.hello ="Hello World";
 
   }])
   .controller('MyCtrl2', ['$scope', function($scope) {
-
+		$scope.hello ="Hello World";
+  }]).controller('MyCtrl3', ['$scope', function($scope) {
+		$scope.hello ="Hello sid";
   }]);
+  
