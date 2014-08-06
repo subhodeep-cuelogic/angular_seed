ea063e32584378a637ebe444b6df68e2da2ab3ac
subhodeep-cuelogic <subhodeep.majumder@cuelogic.co.in>
2014-08-06 18:28:04 +0530

    Completed upto 3rd assignment

--- app/js/app.js
+++ app/js/app.js
@@ -10,8 +10,10 @@ angular.module('myApp', [
   'myApp.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-  $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-  $routeProvider.when('/view3', {templateUrl: 'partials/partial3.html', controller: 'MyCtrl3'});
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'homeCtrl'});		
+  $routeProvider.when('/about-us', {templateUrl: 'partials/about_us.html', controller: 'aboutCtrl'});
+  $routeProvider.when('/join-us', {templateUrl: 'partials/join_us.html', controller: 'joinCtrl'});
+  $routeProvider.when('/cuelogic/clients', {templateUrl: 'partials/clients.html', controller: 'clientCtrl'});
+  $routeProvider.when('/terms-and-conditions', {templateUrl: 'partials/terms_and_conditions.html', controller: 'termsCtrl'});
+  $routeProvider.otherwise({redirectTo: '/home'});
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,14 +2,34 @@
 
 /* Controllers */
 
+  
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
-  	   $scope.hello ="Hello World";
-
+  
+  .controller('headerCtrl', ['$scope', '$location', function($scope, $location) {
+    $scope.loc = $location;
+    $scope.newloc = $location;
+    //$scope.jim = "HI JIM";
+    //$scope.name = "Subhodeep";
+    
   }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
-		$scope.hello ="Hello World";
-  }]).controller('MyCtrl3', ['$scope', function($scope) {
-		$scope.hello ="Hello sid";
-  }]);
+
+    .controller('homeCtrl', ['$scope', '$location', function($scope, $location) {
+  	   $scope.hello ="Hello World";
+       $scope.$location = $location;
+    }])
+    .controller('aboutCtrl', ['$scope', '$location', function($scope, $location) {
+  	   $scope.showText ="aboutCtrl";
+       $scope.$location = $location;
+       //console.log($location.path());
+    }])
+
+    .controller('joinCtrl', ['$scope', '$location', function($scope, $location) {
+		    $scope.showText ="joinCtrl";
+        $scope.$location = $location;
+    }])
+
+    .controller('clientCtrl', ['$scope', '$location', function($scope, $location) {
+		    $scope.showText ="clientCtrl";
+        $scope.$location = $location;
+    }]);
   
